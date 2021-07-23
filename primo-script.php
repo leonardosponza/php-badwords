cc88f20c91b30b98956324170561e47e9bf143cf
leonardo <sponzal4@gmail.com>
2021-07-23 16:46:16 +0200

    get

--- primo-script.php
+++ primo-script.php
@@ -1,5 +1,27 @@
 <?php
+    $testo = 'Trentatré trentini
 
+    entrarono in Trento
+    
+    tutti e trentatré
+    
+    trotterellando
+    
+    trecentotrentatré trentini
+    
+    entrarono in Trento
+    
+    tutti e trentatré
+    
+    trotterellando
+    
+    tremilatrecentotrentatré trentini
+    
+    entrarono in Trento
+    
+    tutti e trentatré
+    
+    trotterellando';
 ?>
 
 <!DOCTYPE html>
@@ -8,9 +30,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>badwords</title>
 </head>
 <body>
-    
+    <p>testo:<?php echo $testo ?> </p>
 </body>
 </html>
